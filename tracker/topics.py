38d9f1782263f8a96abed66d9c385ee4ffec843a
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2024-01-15 19:29:52 +0000

    feat(tracker): drop redundant topics
    Drop Knowledge Distillation and Neural Representations

--- tracker/topics.py
+++ tracker/topics.py
@@ -5,8 +5,6 @@ from typing import List
 ALL_TOPICS: List[str] = [
     "tracker-self-supervised-learning",
     "tracker-geometric-learning",
-    "tracker-knowledge-distillation",
-    "tracker-implicit-representations",
     "tracker-misc",
     "tracker-temp",
 ]
